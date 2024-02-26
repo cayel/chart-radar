899060d513a59455bd600be711d61789117136d7
Christophe Ayel <christophe.ayel@gmail.com>
2024-02-26 23:05:02 +0100

    stats

--- admin/app.py
+++ admin/app.py
@@ -1,14 +1,39 @@
 import streamlit as st
-from mock_data import create_df_ranking
+import sqlite3
+import pandas as pd
 
-# Define the sidebar
-sidebar = st.sidebar
+def load_ranking(connection):
+    df_ranking = pd.read_sql_query("SELECT * FROM ranking", connection)
+    return df_ranking
 
-# Define the sidebar menu
-menu = ["Tableau de bord"]
-choice = sidebar.selectbox("Menu", menu)
+def load_album(connection):
+    df_album = pd.read_sql_query("SELECT * FROM album LEFT JOIN discogs on id_discogs=id", connection)
+    # Rename columns
+    df_album.rename(columns={'artist': 'artist_old'}, inplace=True)
+    df_album.rename(columns={'title': 'title_old'}, inplace=True)
+    return df_album
 
-# Set "Tableau de bord" as the default menu
-if choice == "Tableau de bord":
-    df = create_df_ranking()
-    st.dataframe(df, width=1000, height=500)
+# Create a connection to the SQLite database
+database_path = "../data/database.db"
+connection = sqlite3.connect(database_path)
+
+# Load the ranking data from the database
+df_ranking = load_ranking(connection)
+df_album = load_album(connection)
+
+# Close the database connection
+connection.close()
+
+count_albums = df_album.shape[0]
+count_albums_with_discogs = df_album[df_album['id_discogs'] > 0].shape[0]
+count_albums_with_image = df_album[df_album['image'].notnull()].shape[0]
+count_albums_without_image = df_album[df_album['image'].isnull()].shape[0]
+percentage_albums_with_discogs = 100*(count_albums_with_discogs / count_albums) 
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Nombres d'albums", count_albums)
+col2.metric("Nombre d'albums avec référence", count_albums_with_discogs, f"{percentage_albums_with_discogs:.0f}%")
+col3.metric("Nombre d'albums avec pochette", count_albums_with_image, f"{100*count_albums_with_image/count_albums:.0f}%")
+
+print(df_album)
+
